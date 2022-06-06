a63362d4fcfb6155652ecf30cd68d4582ac17102
Skining888 <tian_xiaomin888@163.com>
2022-06-06 20:57:25 +0800

    修改测试数组

--- test1.cpp
+++ test1.cpp
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main()
 {
+	//int arr[] = { 9,8,7,6,5,4,3,2,1 };
+	//int arr[] = { 9,5,6,3,2,1,4,7,8 };
+	//int arr[] = { 1,2,3,4,5,6,7,8,9 };
+	int arr[] = { 1,1,3,4,5,6,6,7,7,1 };
 	int a = 100;
 	int b = 180;
 	printf("test3 a + b = %d\n",a+b);
